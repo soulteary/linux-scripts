4b4305fc6e8bf6489383dd64aedc447d9d96e101
Su Yang <soulteary@users.noreply.github.com>
2021-07-05 13:59:20 +0800

    use tsinghua mirror

--- docker-with-mirror.sh
+++ docker-with-mirror.sh
@@ -6,6 +6,7 @@
 apt install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
 
 curl -fsSL https://mirrors.tuna.tsinghua.edu.cn/docker-ce/linux/ubuntu/gpg | sudo apt-key add -
+echo "deb https://mirrors.tuna.tsinghua.edu.cn/docker-ce/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 apt update && apt install -y docker-ce
 
